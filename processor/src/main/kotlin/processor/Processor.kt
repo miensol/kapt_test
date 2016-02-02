17c2a6185fdf7561bf1c888844d5645dc88f6774
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2016-02-02 21:06:46 +0100

    Add supported annotations types

--- processor/src/main/kotlin/processor/Processor.kt
+++ processor/src/main/kotlin/processor/Processor.kt
@@ -1,7 +1,9 @@
 package processor
 
+import javax.annotation.processing.SupportedAnnotationTypes
 import javax.annotation.processing.SupportedSourceVersion
 import javax.lang.model.SourceVersion
 
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
+@SupportedAnnotationTypes("com.querydsl.core.annotations.*")
 class QuerydslAnnotationProcessor : com.querydsl.apt.QuerydslAnnotationProcessor()
