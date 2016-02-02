1285bf286dfd04c8859ff4642cc35230d3b21f07
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2016-02-02 21:06:57 +0100

    Add querying sample

--- sqlmodel/src/main/kotlin/test/Test.kt
+++ sqlmodel/src/main/kotlin/test/Test.kt
@@ -1,6 +1,9 @@
 package test
 
 import com.querydsl.core.annotations.QueryEntity
+import com.querydsl.sql.Configuration
+import com.querydsl.sql.PostgreSQLTemplates
+import com.querydsl.sql.postgresql.PostgreSQLQueryFactory
 
 
 @QueryEntity
@@ -11,6 +14,17 @@ class MyEntity {
 
         fun test() {
             val value = QMyEntity.myEntity;
+
+            val configuration = Configuration(PostgreSQLTemplates())
+            val sqlQueryFactory = PostgreSQLQueryFactory(configuration, {
+                //dataSource.connection ?
+                null
+            })
+
+            val query = sqlQueryFactory.from(QMyEntity.myEntity)
+                    .select(QMyEntity.myEntity.id.max())
+
+            val maxId = query.fetchFirst()
         }
 
     }
